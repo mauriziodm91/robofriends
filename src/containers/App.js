081522cab269f2953bdeb062b8aecc8efa462de2
maurizio <mauriziodm91@gmail.com>
2021-10-12 11:43:08 -0300

    cleaner code

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,8 @@
 import React, {Component} from 'react';
-import CardList from './CardList';
-import Scroll from './Scroll';
-import SearchBox from './SearchBox';
+import CardList from '../components/CardList';
+import Scroll from '../components/Scroll';
+import SearchBox from '../components/SearchBox';
 import './App.css'
-
 
 class App extends Component{
     constructor(){
@@ -27,15 +26,13 @@ class App extends Component{
     }
 
     render() {
-        const filteredRobots = this.state.robots.filter(robot =>{
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const {robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         });
 
-        if(this.state.robots.length === 0){
-           return <h1>Loading</h1>
-        }
-        else{
-            return(
+        return !robots.length?<h1>Loading</h1>
+            :(
                 <div className='tc'>
                     <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange = {this.onSearchChange} />
@@ -45,7 +42,6 @@ class App extends Component{
                 </div>
                 
             );
-        }
         
     }
 }
